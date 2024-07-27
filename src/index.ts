57fabfba7261b59b4d06d344c718591cf7b4904f
Tyler Liu <tyler4long@gmail.com>
2024-07-26 18:01:50 -0700

    Add firstObservance

--- src/index.ts
+++ src/index.ts
@@ -3,35 +3,48 @@ import Year from './year';
 export class Holiday {
   public name: string;
   public getter: (year: number) => Date;
+  public firstObservance: number;
 
-  public constructor(name: string, getter: (year: number) => Date) {
+  public constructor(name: string, getter: (year: number) => Date, firstObservance) {
     this.name = name;
     this.getter = getter;
+    this.firstObservance = firstObservance;
   }
 }
 
 export class Holidays {
-  public static newYearsDay = new Holiday("New Year's Day", (year: number) => new Year(year).newYearsDay);
+  public static newYearsDay = new Holiday("New Year's Day", (year: number) => new Year(year).newYearsDay, 1871);
   public static martinLutherKingJrDay = new Holiday(
     'Martin Luther King Jr. Day',
     (year: number) => new Year(year).martinLutherKingJrDay,
+    1986,
   );
   public static washingtonsBirthday = new Holiday(
     "Washington's Birthday",
     (year: number) => new Year(year).washingtonsBirthday,
+    1885,
   );
-  public static goodFriday = new Holiday('Good Friday', (year: number) => new Year(year).goodFriday);
-  public static memorialDay = new Holiday('Memorial Day', (year: number) => new Year(year).memorialDay);
+  public static goodFriday = new Holiday('Good Friday', (year: number) => new Year(year).goodFriday, 1920);
+  public static memorialDay = new Holiday('Memorial Day', (year: number) => new Year(year).memorialDay, 1971);
   public static juneteenthNationalIndependenceDay = new Holiday(
     'Juneteenth National Independence Day',
     (year: number) => new Year(year).juneteenthNationalIndependenceDay,
+    2021,
   );
-  public static independenceDay = new Holiday('Independence Day', (year: number) => new Year(year).independenceDay);
-  public static laborDay = new Holiday('Labor Day', (year: number) => new Year(year).laborDay);
-  public static columbusDay = new Holiday('Columbus Day', (year: number) => new Year(year).columbusDay);
-  public static veteransDay = new Holiday('Veterans Day', (year: number) => new Year(year).veteransDay);
-  public static thanksgivingDay = new Holiday('Thanksgiving Day', (year: number) => new Year(year).thanksgivingDay);
-  public static christmasDay = new Holiday('Christmas Day', (year: number) => new Year(year).christmasDay);
+  public static independenceDay = new Holiday(
+    'Independence Day',
+    (year: number) => new Year(year).independenceDay,
+    1870,
+  );
+  public static laborDay = new Holiday('Labor Day', (year: number) => new Year(year).laborDay, 1894);
+  public static columbusDay = new Holiday('Columbus Day', (year: number) => new Year(year).columbusDay, 1937);
+  public static veteransDay = new Holiday('Veterans Day', (year: number) => new Year(year).veteransDay, 1954);
+  public static thanksgivingDay = new Holiday(
+    'Thanksgiving Day',
+    (year: number) => new Year(year).thanksgivingDay,
+    1941,
+  );
+  public static christmasDay = new Holiday('Christmas Day', (year: number) => new Year(year).christmasDay, 1870);
 
   public static all = [
     Holidays.newYearsDay,
